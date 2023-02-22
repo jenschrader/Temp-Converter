7449b74418919e1c0539753763d377428ae634b6
jenschrader <126116482+jenschrader@users.noreply.github.com>
2023-02-22 16:29:49 -0500

    Update sdev140_temp_convert.py
    format

--- sdev140_temp_convert.py
+++ sdev140_temp_convert.py
@@ -44,8 +44,8 @@ class ConvertTempGui:
         self.MainWindow = tkinter.Tk()
         self.MainWindow.title('Temperature Converter')
         self.MainWindow.geometry('800x700+350+50')
-        # self.MainWindow.maxsize(800, 600)
         self.MainWindow.configure(bg='#bac3f1')
+        
         # dynamic resizing
         # for x in range(1):
         #     self.MainWindow.columnconfigure(x, weight=1, minsize=100)
@@ -53,41 +53,82 @@ class ConvertTempGui:
         #     for y in range(1):
         #         self.WindowFrame = tkinter.Frame(self.MainWindow, relief='ridge', borderwidth=1, bg='#bac3f1')
         #     self.WindowFrame.grid(row=x, column=y, padx=10, pady=10)
+        
         # string var
         self.ConversionValue = tkinter.StringVar()
         # *************************************
 
         # *-- change icon image for window *--
         # *************************************
+        
         # create object of photoimage class
         IconImage = tkinter.PhotoImage(file='temp_icon.png')
+        
         # set icon for window
-        # self.MainWindow.iconbitmap('temp_icon.ico')
         self.MainWindow.iconphoto(False, IconImage)
+        
+        # *************************************
 
         # *-- frames --*
         # *************************************
+        
         self.TopFrame = tkinter.Frame(self.MainWindow, bg='#bac3f1')
+        
         self.MiddleFrame = tkinter.Frame(self.MainWindow, bg='#bac3f1')
+        
         self.BottomFrame = tkinter.Frame(self.MainWindow, bg='#bac3f1')
+        
         # frame placement
         self.TopFrame.pack(side='top', expand=True)
+        
         self.MiddleFrame.pack(side='top', expand=True)
+        
         self.BottomFrame.pack(side='bottom', expand=True)
+        
+        # *************************************
 
         # *-- labels --*
         # *************************************
-        # self.TitleLabel = txt.DynamicLabel(self.TopFrame, text='Temperature Converter',  font=('Silkscreen Regular', 40), bg='#bac3f1', fg='white', justify='center')
-        # self.HeadingLabel = txt.DynamicLabel(self.TopFrame, text='Enter a temperature below', font=('Silkscreen regular', 20), bg='#bac3f1', fg='white', justify='center')
-        # self.FahrLabel = txt.DynamicLabel(self.MiddleFrame, text='Convert to Fahrenheit', font=('Silkscreen Regular', 16), bg='#bac3f1', fg='white', wraplength=200, justify='center')
-        # self.CelsLabel = txt.DynamicLabel(self.MiddleFrame, text='Convert to Celsius', font=('Silkscreen Regular', 16), bg='#bac3f1', fg='white', wraplength=200, justify='center')
-        # self.DisplayTemp = txt.DynamicLabel(self.BottomFrame, textvariable=self.ConversionValue, font=('VT323', 80),
         
-        self.TitleLabel = tkinter.Label(self.TopFrame, text='Temperature Converter',  font=('Silkscreen Regular', 40), bg='#bac3f1', fg='white', justify='center')
-        self.HeadingLabel = tkinter.Label(self.TopFrame, text='Enter a temperature below', font=('Silkscreen regular', 20), bg='#bac3f1', fg='white', justify='center')
-        self.FahrLabel = tkinter.Label(self.MiddleFrame, text='Convert to Fahrenheit', font=('Silkscreen Regular', 16), bg='#bac3f1', fg='white', wraplength=200, justify='center')
-        self.CelsLabel = tkinter.Label(self.MiddleFrame, text='Convert to Celsius', font=('Silkscreen Regular', 16), bg='#bac3f1', fg='white', wraplength=200, justify='center')
-        self.DisplayTemp = tkinter.Label(self.BottomFrame, textvariable=self.ConversionValue, font=('VT323', 80), bg='#bac3f1', fg='white')
+        self.TitleLabel = tkinter.Label(
+            self.TopFrame,
+            text='Temperature Converter',
+            font=('Silkscreen Regular', 40),
+            bg='#bac3f1',
+            fg='white',
+            justify='center')
+        
+        self.HeadingLabel = tkinter.Label(
+            self.TopFrame,
+            text='Enter a temperature below',
+            font=('Silkscreen regular', 20),
+            bg='#bac3f1',
+            fg='white',
+            justify='center')
+        
+        self.FahrLabel = tkinter.Label(
+            self.MiddleFrame,
+            text='Convert to Fahrenheit',
+            font=('Silkscreen Regular', 16),
+            bg='#bac3f1',
+            fg='white',
+            wraplength=200,
+            justify='center')
+        
+        self.CelsLabel = tkinter.Label(
+            self.MiddleFrame,
+            text='Convert to Celsius',
+            font=('Silkscreen Regular', 16),
+            bg='#bac3f1', fg='white',
+            wraplength=200,
+            justify='center')
+        
+        self.DisplayTemp = tkinter.Label(
+            self.BottomFrame,
+            textvariable=self.ConversionValue,
+            font=('VT323', 80),
+            bg='#bac3f1',
+            fg='white')
 
         # label placement
         self.TitleLabel.grid(row=0, column=0, pady=50)
@@ -95,6 +136,7 @@ class ConvertTempGui:
         self.FahrLabel.grid(row=1, column=0, padx=10, pady=2)
         self.CelsLabel.grid(row=1, column=1, padx=20, pady=2)
         self.DisplayTemp.grid(row=0, column=0)
+        
         # alternate:
         # self.HeadingLabel.pack(side='top', fill='both', expand=True)
         # self.TitleLabel.pack(side='top', fill='both', expand=True)
@@ -102,13 +144,20 @@ class ConvertTempGui:
         # self.CelsLabel.pack(anchor='ne', expand=True, side='left')
         # self.DisplayTemp.pack(side='bottom', expand=True, fill='both')
         # self.EntryBoxLabel.grid(row=2, column=1)
+        
         # *************************************
 
         # *-- text entry --*
         # *************************************
-        self.TempEntry = tkinter.ttk.Entry(self.TopFrame, font=('VT323 20'), width=4, justify='center')
+        self.TempEntry = tkinter.ttk.Entry(
+            self.TopFrame,
+            font=('VT323 20'),
+            width=4,
+            justify='center')
+        
         # entry placement
         self.TempEntry.grid(row=2, column=0, pady=20)
+        
         # alternate:
         # self.TempEntry.pack(side='top', expand=True)
         # *************************************
@@ -117,16 +166,31 @@ class ConvertTempGui:
         # *************************************
         # loading/resizing button pics
         self.FahrImage = tkinter.PhotoImage(file='f_btn2.png')
+        
         self.CelsImage = tkinter.PhotoImage(file='c_btn1.png')
+        
         self.ResizeFahrImage = self.FahrImage.subsample(2, 2)
+        
         self.ResizeCelsImage = self.CelsImage.subsample(2, 2)
 
         # create buttons
-        self.ConvertFahr = tkinter.Button(self.MiddleFrame, image=self.FahrImage, bg='#bac3f1', border=0)
-        self.ConvertCels = tkinter.Button(self.MiddleFrame, image=self.CelsImage, bg='#bac3f1', border=0)
+        self.ConvertFahr = tkinter.Button(
+            self.MiddleFrame,
+            image=self.FahrImage,
+            bg='#bac3f1',
+            border=0)
+        
+        self.ConvertCels = tkinter.Button(
+            self.MiddleFrame,
+            image=self.CelsImage,
+            bg='#bac3f1',
+            border=0)
+        
         # button placement
         self.ConvertFahr.grid(row=2, column=0)
+        
         self.ConvertCels.grid(row=2, column=1)
+        
         # alternate:
         # self.ConvertFahr.pack(side='left', expand=True, fill='both')
         # self.ConvertCels.pack(side='right', expand=True, fill='both')
@@ -136,8 +200,10 @@ class ConvertTempGui:
         # *************************************
         self.ConvertFahr.bind('<Button-1>', self.ConvertToFahrenheit)
         self.ConvertFahr.focus()
+        
         self.ConvertCels.bind('<Button-1>', self.ConvertToCelsius)
         self.ConvertCels.focus()
+        
         # self.MainWindow.bind('<Configure>', self.DynamicLabel)
 
         # *************************************
@@ -152,8 +218,10 @@ class ConvertTempGui:
     def ConvertToFahrenheit(self, event):
         # get input
         self.UserTempEntry = float(self.TempEntry.get())
+        
         # convert
         self.FahrenheitTemp = self.UserTempEntry * 1.8 + 32
+        
         # store new temp in string var and format
         self.ConversionValue.set(f'{self.FahrenheitTemp:.1f}' + u'F\N{DEGREE SIGN}')
     # *************************************
@@ -163,8 +231,10 @@ class ConvertTempGui:
     def ConvertToCelsius(self, event):
         # get input
         self.UserTempEntry = float(self.TempEntry.get())
+        
         # convert
         self.CelsiusTemp = (self.UserTempEntry - 32) * .5556
+        
         # store new temp in string var and format
         self.ConversionValue.set(f'{self.CelsiusTemp:.1f}' + u'C\N{DEGREE SIGN}')
     # *************************************
